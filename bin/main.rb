42b47a8153ca2e5f2a1e3870750b05e7df3b8d3c
Muhammad Fahad E Umer <eng.fahadumer@gmail.com>
2020-05-14 01:25:10 +0500

    Stickler Errors

--- bin/main.rb
+++ bin/main.rb
@@ -3,7 +3,7 @@
 puts 'Welcome to Tic Tac TOE'
 puts 'Please Read All Rules Carefully'
 puts 'To Acquire your box you have to select the number from 1 - 9'
-my_array = [[1,2,3], [4,5,6], [7,8,9]]
+my_array = [[1, 2, 3], [4, 5, 6], [7, 8,9]]
 my_array.each do |row|
   print row.join(' | ')
   puts
@@ -14,7 +14,7 @@ first_player = gets.chomp
 puts 'Please Enter Second Player Name'
 second_player = gets.chomp
 puts "#{first_player} has symbol X and #{second_player} has been assigned symbol O"
-my_array = [['-','-','-'], ['-','-','-'], ['-','-','-']]
+my_array = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]
 my_array.each do |row|
   print row.join(' | ')
   puts
