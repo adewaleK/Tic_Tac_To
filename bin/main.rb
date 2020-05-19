7b755350fb30db89f1d7a83aff6c1e115bd87262
Muhammad Fahad E Umer <eng.fahadumer@gmail.com>
2020-05-19 20:53:44 +0500

    Logic and Board class added

--- bin/main.rb
+++ bin/main.rb
@@ -1,29 +1,43 @@
 #!/usr/bin/env ruby
+require_relative ('../lib/board.rb')
+require_relative ('../lib/logic.rb')
 
 puts 'Welcome to Tic Tac TOE'
 puts 'Please Read All Rules Carefully'
 puts 'To Acquire your box you have to select the number from 1 - 9'
-my_array = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-my_array.each do |row|
-  print row.join(' | ')
-  puts
-end
+puts 'Sample is given below'
+sample_array = [1,2,3,4,5,6,7,8,9]
+new_board = Board.new
+new_board.display_board(sample_array)
 puts
-puts 'Please Enter First Player Name'
-first_player = gets.chomp
-puts 'Please Enter Second Player Name'
-second_player = gets.chomp
-puts "#{first_player} has symbol X and #{second_player} has been assigned symbol O"
-my_array = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]
-my_array.each do |row|
-  print row.join(' | ')
-  puts
+puts 'Your game Starts Now'
+puts
+new_board.display_board
+puts
+game = Tic_Tac_Logic.new
+player_symbols = ["X", "O"]
+player_turn = 1
+play_game = false
+while play_game == false
+print "Player #{player_turn}: Please Select your box (1-9): "
+box_selection = gets.chomp.to_i
+game_progress = game.check(player_symbols[player_turn - 1], box_selection)
+if player_turn == 1 && game_progress[0] == true
+  player_turn = 2
+elsif player_turn == 2 && game_progress[0] == true
+  player_turn = 1
+else
+  puts "\n Please Select the right move \n"
 end
-puts 'Your game start Now'
-puts "#{first_player}: Please Select your box (1-9)"
-first_player_box_selection = gets.chomp
-puts "#{second_player}: Please Select your box (1-9)"
-second_player_box_selection = gets.chomp
-
-puts first_player_box_selection
-puts second_player_box_selection
+game_progress_array = game_progress[1]
+game_progress_array
+puts
+new_board.display_board(game_progress_array)
+if game.win_status[0] == true
+  puts "Congratulations!!! Player with '#{game.win_status[1]}' Wins"
+  play_game = true
+elsif game.draw_status == true
+  puts "Game Draws"
+  play_game = true
+end
+end
